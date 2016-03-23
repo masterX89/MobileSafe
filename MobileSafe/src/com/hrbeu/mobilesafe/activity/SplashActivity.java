0b4695162539a3623c54b1ef177d7f5959377e01
Hankai Xia <xhk1995@gmail.com>
2016-03-23 19:49:41 +0800

    splash

---
+++ MobileSafe/src/com/hrbeu/mobilesafe/activity/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.hrbeu.mobilesafe.activity;
+
+import com.hrbeu.mobilesafe.R;
+import com.hrbeu.mobilesafe.R.id;
+import com.hrbeu.mobilesafe.R.layout;
+import com.hrbeu.mobilesafe.R.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
