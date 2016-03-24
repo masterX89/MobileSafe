4de0997bbb854877a9425d809313c4e59674e829
Hankai Xia <xhk1995@gmail.com>
2016-03-24 22:22:10 +0800

    splash
    动态获取versionName

--- MobileSafe/src/com/hrbeu/mobilesafe/activity/SplashActivity.java
+++ MobileSafe/src/com/hrbeu/mobilesafe/activity/SplashActivity.java
@@ -1,23 +1,42 @@
 package com.hrbeu.mobilesafe.activity;
 
-import com.hrbeu.mobilesafe.R;
-import com.hrbeu.mobilesafe.R.id;
-import com.hrbeu.mobilesafe.R.layout;
-import com.hrbeu.mobilesafe.R.menu;
-
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.hrbeu.mobilesafe.R;
 
 public class SplashActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private TextView tvVersion;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		tvVersion = (TextView) findViewById(R.id.tv_version);
+		tvVersion.setText("°æ±¾ºÅ:"+getVersionName());
+	}
+
+	private String getVersionName() {
+
+		PackageManager packageManager = getPackageManager();
+
+		try {
+			PackageInfo packageInfo = packageManager.getPackageInfo(
+					getPackageName(), 0);
+			int versionCode = packageInfo.versionCode;
+			String versionName = packageInfo.versionName;
+			return versionName;
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
 
 }
