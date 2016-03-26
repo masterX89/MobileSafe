895fc689b9dbde13d0e164320dfdef9b2eb87f28
Hankai Xia <xhk1995@gmail.com>
2016-03-26 20:21:06 +0800

    获取响应码

--- MobileSafe/src/com/hrbeu/mobilesafe/activity/SplashActivity.java
+++ MobileSafe/src/com/hrbeu/mobilesafe/activity/SplashActivity.java
@@ -1,5 +1,11 @@
 package com.hrbeu.mobilesafe.activity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -19,9 +25,14 @@ public class SplashActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		tvVersion = (TextView) findViewById(R.id.tv_version);
-		tvVersion.setText("version:"+getVersionName());
+		tvVersion.setText("版本号:" + getVersionName());
 	}
 
+	/**
+	 * 获取版本名称
+	 * 
+	 * @return 版本名称
+	 */
 	private String getVersionName() {
 
 		PackageManager packageManager = getPackageManager();
@@ -33,10 +44,48 @@ public class SplashActivity extends Activity {
 			String versionName = packageInfo.versionName;
 			return versionName;
 		} catch (NameNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return "";
+	}
+
+	/**
+	 * 从服务器获取版本信息进行校验
+	 */
+	private void checkVersion() {
+		// 启动子线程异步加载
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					// 本机地址：localhost，模拟器访问本机地址：10.0.2.2
+					URL url = new URL("http://10.0.2.2:8080/update.json");
+					HttpURLConnection conn = (HttpURLConnection) url
+							.openConnection();
+					// 设置请求方法：GET
+					conn.setRequestMethod("GET");
+					// 设置连接超时
+					conn.setConnectTimeout(5000);
+					// 设置响应超时
+					conn.setReadTimeout(5000);
+					// 连接服务器
+					conn.connect();
+					// 获取响应码
+					int responseCode = conn.getResponseCode();
+					if (responseCode == 200) {
+						InputStream inputStream = conn.getInputStream();
+					}
+				} catch (MalformedURLException e) {
+					// URL错误
+					e.printStackTrace();
+				} catch (IOException e) {
+					// 网络错误
+					e.printStackTrace();
+				}
+			}
+
+		}.start();
+
 	}
 
 }
