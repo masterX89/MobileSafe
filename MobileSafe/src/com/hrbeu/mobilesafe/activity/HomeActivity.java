f596f00842ae280fb0cd47f15869bebd6167675e
Hankai Xia <xhk1995@gmail.com>
2016-03-31 22:10:18 +0800

    主界面布局

--- MobileSafe/src/com/hrbeu/mobilesafe/activity/HomeActivity.java
+++ MobileSafe/src/com/hrbeu/mobilesafe/activity/HomeActivity.java
@@ -2,6 +2,10 @@ package com.hrbeu.mobilesafe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 
 import com.hrbeu.mobilesafe.R;
 
@@ -13,9 +17,38 @@ import com.hrbeu.mobilesafe.R;
  */
 public class HomeActivity extends Activity {
 
+	private GridView gvHome;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+
+		gvHome = (GridView) findViewById(R.id.gv_home);
+		gvHome.setAdapter(null);
+	}
+
+	class HomeAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			return null;
+		}
+
 	}
 }
