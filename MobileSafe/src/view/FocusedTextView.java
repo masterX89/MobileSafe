84fb729445d5907fc39e6dc413b967b3ad90b0ad
Hankai Xia <xhk1995@gmail.com>
2016-04-02 20:09:54 +0800

    textView滚动功能实现

--- MobileSafe/src/view/FocusedTextView.java
+++ MobileSafe/src/view/FocusedTextView.java
@@ -2,11 +2,18 @@ package view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewDebug.ExportedProperty;
 import android.widget.TextView;
 
+/**
+ * 获取焦点的TextView
+ * 
+ * @author Hankai Xia
+ * 
+ */
 public class FocusedTextView extends TextView {
 	/**
-	 * 有style样式的时候走此方法
+	 * 有style样式的时候用此方法
 	 * 
 	 * @param context
 	 * @param attrs
@@ -17,7 +24,7 @@ public class FocusedTextView extends TextView {
 	}
 
 	/**
-	 * 有属性时走此方法
+	 * 有属性时用此方法
 	 * 
 	 * @param context
 	 * @param attrs
@@ -26,8 +33,21 @@ public class FocusedTextView extends TextView {
 		super(context, attrs);
 	}
 
+	/**
+	 * 用代码new对象时，用此方法
+	 * 
+	 * @param context
+	 */
 	public FocusedTextView(Context context) {
 		super(context);
 	}
 
+	/**
+	 * 表示有没有获取焦点 跑马灯要运行，首先调用调用此函数判断是否有焦点，是true则有效果
+	 */
+	@Override
+	@ExportedProperty(category = "focus")
+	public boolean isFocused() {
+		return true;
+	}
 }
